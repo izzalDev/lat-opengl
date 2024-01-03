aafd8ef6a049d7f93f3d8ba80a9800a6cd2f0401
Rizal Fadlullah <rizal.fadlullah@gmail.com>
2024-01-04 02:49:47 +0700

    Initial Commit

---
+++ coba/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coba
+//
+//  Created by Rizal Fadlullah on 04/01/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
